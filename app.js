8cb952e3a19152ad2d9341f96ac4c39d3702eef1
Diogo Lopes <deosl98@gmail.com>
2021-03-15 17:46:03 +0000

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const countDownDate = new Date('6 July, 2021 00:00:00').getTime();
 
-const x = setInterval(function() {
+const x = setInterval(function () {
     const now = new Date().getTime();
     const distance = countDownDate - now;
 
@@ -9,12 +9,12 @@ const x = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('1').innerHTML = days;
-    document.getElementById('2').innerHTML = formatTime(hours);
-    document.getElementById('3').innerHTML = formatTime(minutes);
-    document.getElementById('4').innerHTML = formatTime(seconds);
+    document.getElementById('one').innerHTML = days;
+    document.getElementById('two').innerHTML = formatTime(hours);
+    document.getElementById('three').innerHTML = formatTime(minutes);
+    document.getElementById('four').innerHTML = formatTime(seconds);
 
-    if(distance < 0) {
+    if (distance < 0) {
         clearInterval(x);
     }
 }, 1000);
@@ -22,3 +22,9 @@ const x = setInterval(function() {
 function formatTime(time) {
     return time < 10 ? (`0${time}`) : time;
 }
+
+const one = document.getElementById('one')
+
+one.addEventListener('click', () => {
+    console.log('wtf!');
+});
